9e1fed2d1b8b5aeb6063c0f8c5228e5ad4352027
aurelioo29 <aureliolucio5@gmail.com>
2024-10-07 16:54:24 +0700

    Add dummy data section

---
+++ src/data/faqData.js
@@ -0,0 +1,33 @@
+export const faqs = [
+  {
+    question:
+      "What prior knowledge do I need before enrolling in this program?",
+    answer:
+      "A basic understanding of data concepts is essential for success in this program. Familiarity with terms and principles related to data will help you grasp the course material more effectively.",
+  },
+  {
+    question: "What tools will I learn?",
+    answer:
+      "You will learn Python, SQL, data visualization tools, and much more.",
+  },
+  {
+    question: "How long does the bootcamp last?",
+    answer: "The bootcamp lasts for 12 weeks of intensive learning.",
+  },
+  {
+    question: "Will I receive a certificate?",
+    answer:
+      "Yes, upon completion of the course, you will receive a certificate.",
+  },
+  {
+    question: "Do I need to have programming skills to participate?",
+    answer:
+      "While basic programming and algorithm knowledge can be beneficial, they are not mandatory for enrollment. However, being open to learning programming concepts will enhance your experience.",
+  },
+  {
+    question:
+      "What personal qualities should I have to succeed in this program?",
+    answer:
+      "Commitment and perseverance are crucial. Data-related fields can be challenging, and having the determination to overcome obstacles will greatly contribute to your success in this program.",
+  },
+];
---
+++ src/data/linkData.js
@@ -0,0 +1,8 @@
+export const linksData = [
+  { href: "#", label: "Method of Delivery" },
+  { href: "#", label: "Technical Requirements" },
+  { href: "#", label: "Kurikulum" },
+  { href: "#methodstudy", label: "Journey Program" },
+  { href: "#timeprice", label: "Jadwal & Biaya" },
+  { href: "#faq", label: "FAQ" },
+];
