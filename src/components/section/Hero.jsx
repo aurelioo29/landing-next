7a56f4901a6b2648bb02dfd1d49ff5b007af963d
aurelioo29 <aureliolucio5@gmail.com>
2024-10-08 16:20:23 +0700

    feat(Respon): Add feature responsive on Hero section

--- src/components/section/Hero.jsx
+++ src/components/section/Hero.jsx
@@ -15,21 +15,24 @@ export default function Hero() {
   return (
     <Box
       bg="gray.100"
-      h="65vh"
+      h={{ base: "auto", md: "65vh" }}
       display="flex"
       justifyContent="center"
       alignItems="center"
       textAlign="center"
       px={4}
+      py={{ base: 8, md: 0 }}
     >
       <Flex
         w="full"
         maxW="1200px"
+        direction={{ base: "column", md: "row" }}
         justifyContent="space-between"
         alignItems="center"
         px={6}
       >
-        <Box textAlign="left" maxW="600px">
+        <Box textAlign="left" maxW="600px" mb={{ base: 6, md: 0 }}>
+          {" "}
           <Heading as="h1" size="2xl" mb={4}>
             Transform Your Career with{" "}
             <Text as="span" color="green.400">
@@ -41,8 +44,11 @@ export default function Hero() {
             Data Science, and Data Engineering experts. Gain the skills to
             thrive in today&apos;s data-driven world.
           </Text>
-
-          <HStack spacing={4}>
+          <HStack
+            spacing={4}
+            justifyContent={{ base: "center", md: "flex-start" }}
+          >
+            {" "}
             <Link href="#contact">
               <Button colorScheme="green" size="lg">
                 Contact Us
@@ -58,7 +64,7 @@ export default function Hero() {
           <Image
             src="/assets/hero_section.jpg"
             alt="Hero Section"
-            boxSize="470px"
+            boxSize={{ base: "100%", md: "470px" }}
             objectFit="cover"
             borderRadius="2xl"
             className={Style.heroImage}
